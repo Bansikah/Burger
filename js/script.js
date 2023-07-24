04e021d35587c5a97e7d0ea634887b7908c366d0
Bansikah <tandapnoelbansikah@gmail.com>
2023-07-24 23:57:54 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+const toTop = document.querySelector(".to-top");
+
+let cookieModal = document.querySelector(".cookie-consent-modal");
+let cancelCookieBtn = document.querySelector(".btn.cancel");
+let acceptCookieBtn = document.querySelector(".btn.accept");
+
+menu.addEventListener("click", function(){
+   navbar.classList.toggle("active");
+});
+
+window.onscroll = () => {
+    navbar.classList.remove("active");
+};
+
+window.addEventListener("scroll", () => {
+    if(window.pageYOffset > 100){
+        toTop.classList.add("active");
+    }
+    else{
+        toTop.classList.remove("active");
+    }
+})
+
+cancelCookieBtn.addEventListener("click", function(){
+     cookieModal.classList.remove("active");
+})
+
+acceptCookieBtn.addEventListener("click", function(){
+    cookieModal.classList.remove("active");
+    localStorage.setItem("cookieAccepted", "yes");
+})
+ 
+setTimeout(function (){
+    let cookieAccepted = localStorage.getItem("cookieAccept")
+    if(cookieAccepted != "yes"){
+        cookieModal.classList.add("active");
+    }
+}, 2000)
