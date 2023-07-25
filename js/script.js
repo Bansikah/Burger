a6c35cf820832880a9b17089ae375fcae1690f21
Bansikah <tandapnoelbansikah@gmail.com>
2023-07-25 11:42:43 +0100

    changed cookie section in index.html

--- js/script.js
+++ js/script.js
@@ -3,9 +3,8 @@ let navbar = document.querySelector(".navbar");
 
 const toTop = document.querySelector(".to-top");
 
-let cookieModal = document.querySelector(".content");
-let cancelCookieBtn = document.querySelector(".btn.cancel");
-let acceptCookieBtn = document.querySelector(".btn.accept");
+const 
+
 
 menu.addEventListener("click", function(){
    navbar.classList.toggle("active");
@@ -24,18 +23,4 @@ window.addEventListener("scroll", () => {
     }
 })
 
-cancelCookieBtn.addEventListener("click", function(){
-     cookieModal.classList.remove("active");
-})
-
-acceptCookieBtn.addEventListener("click", function(){
-    cookieModal.classList.remove("active");
-    localStorage.setItem("cookieAccepted", "yes");
-})
  
-setTimeout(function (){
-    let cookieAccepted = localStorage.getItem("cookieAccept")
-    if(cookieAccepted != "yes"){
-        cookieModal.classList.add("active");
-    }
-}, 2000)
